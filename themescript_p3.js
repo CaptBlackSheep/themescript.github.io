582ff54c6ca38da8f5fb0d668ed0cd86d7e3ce87
Wizzikz <rihikz@gmail.com>
2015-04-30 18:08:35 +0300

    Initial upload / Transfer to new repo

---
+++ themescript_p3.js
@@ -0,0 +1,12 @@
+$.getScript("https://d1rfegul30378.cloudfront.net/files/plugCubed.js",       
+            
+            
+            function(){
+
+});
+            
+                        
+ $.getScript("https://wizzikz.github.io/themescript.js",           
+            function(){
+
+});
---
+++ themescript_rcs.js
@@ -0,0 +1,12 @@
+$.getScript("https://code.radiant.dj/rs.min.js",       
+            
+            
+            function(){
+
+});
+            
+                        
+ $.getScript("https://wizzikz.github.io/themescript.js",           
+            function(){
+
+});
