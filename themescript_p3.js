87a0d01506bd851fdf73a7cdcf6e56e46fd3a47d
PDKnight <backsuit@szm.sk>
2015-05-02 14:14:59 +0200

    Update themescript_p3.js

--- themescript_p3.js
+++ themescript_p3.js
@@ -1,12 +1,2 @@
-$.getScript("https://d1rfegul30378.cloudfront.net/files/plugCubed.js",       
-            
-            
-            function(){
-
-});
-            
-                        
- $.getScript("https://themescript.github.io/themescript.js",           
-            function(){
-
-});
+$.getScript("https://d1rfegul30378.cloudfront.net/files/plugCubed.js");
+$.getScript("https://themescript.github.io/themescript.js");
